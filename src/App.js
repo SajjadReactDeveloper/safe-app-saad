4d76ecf1f8e4f8e5f29a6e2125d2bde438d7d034
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2024-03-29 11:26:32 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useMemo } from 'react';
 import { ethers } from 'ethers';
-import { SafeProvider, useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
+import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
 import { SafeAppProvider } from '@safe-global/safe-apps-provider';
 
 function App() {
-  // This component now uses the useSafeAppsSDK hook within the SafeProvider context
   const { sdk, safe } = useSafeAppsSDK();
   const web3Provider = useMemo(() => new ethers.providers.Web3Provider(new SafeAppProvider(safe, sdk)), [sdk, safe]);
   console.log(web3Provider);
-  // use web3Provider with contracts here
-  // ...
-
   return (
     <div>
       My Safe App
